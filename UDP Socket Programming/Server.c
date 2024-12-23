de31e366bf15310959d298bf1b449a574fe9f873
qkrdudwls <129957194+qkrdudwls@users.noreply.github.com>
2024-12-23 13:58:53 +0900

    Update Server.c

--- UDP Socket Programming/Server.c
+++ UDP Socket Programming/Server.c
@@ -12,27 +12,23 @@
 int main(int argc, char* argv[]) {
     int retval;
 
-    // Winsock 초기화
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         fprintf(stderr, "WSAStartup() failed\n");
         exit(1);
     }
 
-    // 포트 번호 설정
     int PORT = 0;
     printf("Input Server Port : ");
     scanf("%d", &PORT);
     while (getchar() != '\n');
 
-    // socket()
     SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
     if (s == INVALID_SOCKET) {
         fprintf(stderr, "socket() failed\n");
         exit(1);
     }
 
-    // bind()
     SOCKADDR_IN serverAddr;
     ZeroMemory(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
@@ -44,7 +40,6 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // 변수
     SOCKADDR_IN clientAddr;
     int total_bytes = 0;
     int total_messages = 0;
@@ -52,10 +47,9 @@ int main(int argc, char* argv[]) {
     char message[BUFSIZE + 1] = "";
     int clientAddrLength = 0;
     char response[BUFSIZE + 1] = "";
-    char ServerIP[INET_ADDRSTRLEN]; // 서버 IP 주소
-    char ClientIP[INET_ADDRSTRLEN]; // 클라이언트 IP 주소
+    char ServerIP[INET_ADDRSTRLEN]; 
+    char ClientIP[INET_ADDRSTRLEN]; 
 
-    // 호스트 이름 추출
     char hostname[256];
     retval = gethostname(hostname, sizeof(hostname));
     if (retval == SOCKET_ERROR) {
@@ -63,7 +57,6 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // 호스트 이름을 기반으로 IP 주소 구하기 
     struct addrinfo hints, * res = NULL;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -76,14 +69,11 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // IP 주소 변환
     inet_ntop(AF_INET, &((struct sockaddr_in*)res->ai_addr)->sin_addr, ServerIP, sizeof(ServerIP));
 
-    // 메모리 해제 
     freeaddrinfo(res);
 
     while (1) {
-        // 메시지 받기 
         clientAddrLength = sizeof(clientAddr);
         retval = recvfrom(s, message, BUFSIZE, 0, (SOCKADDR*)&clientAddr, &clientAddrLength);
         if (retval == SOCKET_ERROR) {
@@ -94,26 +84,21 @@ int main(int argc, char* argv[]) {
             message[retval] = '\0';
         }
 
-        // 통계 정보 업데이트
         total_messages++;
 
-        // Command_code 추출
         memcpy(&command_code, message, sizeof(command_code));
         command_code = ntohs(*(unsigned short*)message);
 
-        // 통계 정보 업데이트
         total_bytes += retval;
 
-        // 클라이언트 IP 주소 추출
         inet_ntop(AF_INET, &clientAddr.sin_addr, ClientIP, sizeof(ClientIP));
 
         switch (command_code) {
-        case 0x01: // echo
+        case 0x01: 
             printf("Client Message : %s\n", message + 2);
             break;
 
-        case 0x02: // chat
-            // 메시지 입력
+        case 0x02:
             printf("Client Message : %s\n", message + 2);
             printf("Input Message : ");
             fgets(message + 2, BUFSIZE - 2, stdin);
@@ -122,19 +107,16 @@ int main(int argc, char* argv[]) {
             }
             break;
 
-        case 0x03: // stat 
+        case 0x03: 
             if (strcmp(message + 2, "bytes") == 0) {
-                // 정수형 데이터를 문자열로 변환 
                 itoa(total_bytes, message + 2, 10);
                 strcat(message + 2, " bytes");
             }
             else if (strcmp(message + 2, "number") == 0) {
-                // 정수형 데이터를 문자열로 변환
                 itoa(total_messages, message + 2, 10);
                 strcat(message + 2, " messages");
             }
             else {
-                // 정수형 데이터를 문자열로 변환
                 itoa(total_bytes, message + 2, 10);
                 strcat(message + 2, " bytes, ");
                 itoa(total_messages, message + 2 + strlen(message + 2), 10);
@@ -142,8 +124,7 @@ int main(int argc, char* argv[]) {
             }
             break;
 
-        case 0x04: // quit 
-            // 종료 
+        case 0x04: 
             printf("Terminated\n");
             closesocket(s);
             WSACleanup();
@@ -154,7 +135,6 @@ int main(int argc, char* argv[]) {
             continue;
         }
 
-        // 응답 메시지 전송
         snprintf(response, BUFSIZE, "%s:%d가 %s:%d로부터 %d 바이트 메시지 수신: %s", ServerIP - 2, PORT, ClientIP, ntohs(clientAddr.sin_port), retval, message + 2);
         retval = sendto(s, response, strlen(response), 0, (SOCKADDR*)&clientAddr, sizeof(clientAddr));
         if (retval == SOCKET_ERROR) {
